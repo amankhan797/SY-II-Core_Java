2da500dc2bb952096922c7594109ea6e6209a785
Aman Khan <amankhanaezakmi@gmail.com>
2023-04-25 22:46:47 +0530

    Java program to display characters from A to Z using loop.

---
+++ Q8.java
@@ -0,0 +1,9 @@
+// Java program to display characters from A to Z using loop.
+class Q8{
+    public static void main(String[]args){
+        System.out.println("Alphabets From A-Z:");
+        for(int i = 'A' ; i <= 'Z'; i++){
+            System.out.printf("%c ", i);
+        }
+    }
+}
